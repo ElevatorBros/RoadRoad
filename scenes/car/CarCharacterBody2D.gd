3e34355b3eebe11a7d7fa8ca68c4658c0e2b4ec9
OrestesK <oresteskassian@gmail.com>
2023-07-07 16:39:52 -0400

    Cool map and mouse and bounce

--- scenes/car/CarCharacterBody2D.gd
+++ scenes/car/CarCharacterBody2D.gd
@@ -1,9 +1,21 @@
 extends CharacterBody2D
 
+var initial_velocity = Vector2(250, 10)
+
 func _ready():
-	set_velocity(Vector2(250, 250))
+	set_velocity(initial_velocity)
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
 		velocity = velocity.bounce(collision_info.get_normal())
+	
+	rotation = velocity.angle()
+	
+	var mouse = get_global_mouse_position()
+	
+	var bias = 1000 * delta
+	var pos_diff = (mouse - position).normalized()
+	
+	velocity += pos_diff * bias
+		
