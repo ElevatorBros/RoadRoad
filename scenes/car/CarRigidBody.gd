5c37e0c613bfabc0effc81c5122a4352a90e3cf1
Shinji322 <admin@ronanm.me>
2023-07-07 15:47:28 -0400

    transfered workload to orestes

---
+++ scenes/car/CarRigidBody.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	#move_and_collide(Vector2(linear_velocity.x * delta, linear_velocity.y * delta))
