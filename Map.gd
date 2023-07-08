29a771ef7adab1bb54b698b3c23c7fc7789d53d7
Joshua Bourgeot <dragonsploder@gmail.com>
2023-07-07 20:48:27 -0700

    Revert "top and bottom bars connect"
    This reverts commit bca250e63d680f865a46655e062be10470d8d73a.

--- Map.gd
+++ Map.gd
@@ -3,28 +3,25 @@ extends Node2D
 @export var straight_road_scene: PackedScene
 #@onready var straight_road = straight_road_scene.instantiate()
 #@onready var roads = find_child("Roads")
-signal send_points_to_road(a: Vector2, b: Vector2, old_rot: float)
+signal send_points_to_road(a: Vector2, b: Vector2)
 
 var old_mouse_pos = Vector2(300,300)
-var old_road_rot = 0
 
-func add_road(a, b, rot) -> float:
+func add_road(a, b):
 	var straight_road = straight_road_scene.instantiate()
 	print(straight_road)
 	add_child(straight_road)
 	move_child(straight_road, 0)
 	send_points_to_road.connect(straight_road.on_send_points_to_road)
-	send_points_to_road.emit(a, b, rot)
+	send_points_to_road.emit(a, b)
 	send_points_to_road.disconnect(straight_road.on_send_points_to_road)
-	
-	return rot
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#straight_road.global_position = Vector2(600,600)
 	#roads.add_child(straight_road)
 	#roads.move_child(straight_road, -1)
-	add_road(Vector2(0, old_mouse_pos.y), old_mouse_pos, 0)
+	add_road(Vector2(0, old_mouse_pos.y), old_mouse_pos)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -35,10 +32,8 @@ func _input(event):
 #		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 #			add_road(old_mouse_pos, get_global_mouse_position())
 #			old_mouse_pos = get_global_mouse_position()
-	var current_mouse_pos = get_global_mouse_position()
-	if current_mouse_pos.distance_to(old_mouse_pos) > 50:
-		add_road(old_mouse_pos, current_mouse_pos, old_road_rot)
-		old_road_rot = atan2(old_mouse_pos.y - current_mouse_pos.y, old_mouse_pos.x - current_mouse_pos.x)
+	if get_global_mouse_position().distance_to(old_mouse_pos) > 50:
+		add_road(old_mouse_pos, get_global_mouse_position())
 		old_mouse_pos = get_global_mouse_position()
 
 
--- scenes/roads/StraightRoad.gd
+++ scenes/roads/StraightRoad.gd
@@ -1,10 +1,7 @@
 extends Node2D
 
-var start_pos_top = Vector2(0,0)
-var end_pos_top = Vector2(0,0)
-
-var start_pos_bottom = Vector2(0,0)
-var end_pos_bottom = Vector2(0,0)
+var start_pos = Vector2(0,0)
+var end_pos = Vector2(0,0)
 
 var road_x_size = 680
 
@@ -12,28 +9,40 @@ var road_y_scale = 0.4
 
 var indent = 20
 
-func on_send_points_to_road(a: Vector2, b: Vector2, old_rot: float):
-	start_pos_top = a + Vector2(0, -50)
-	end_pos_top = b + Vector2(0, -50)
+func on_send_points_to_road(a: Vector2, b: Vector2):
+	start_pos = a - Vector2(indent, 0)
+	end_pos = b
 	
-	$StaticBody2D/Top.position = start_pos_top + ((end_pos_top - start_pos_top) / 2)
-	$StaticBody2D/Top.rotation = atan2(end_pos_top.y - start_pos_top.y, end_pos_top.x - start_pos_top.x)
-	$StaticBody2D/Top.scale = Vector2(start_pos_top.distance_to(end_pos_top)/road_x_size, road_y_scale)
+	self.position = start_pos + ((end_pos - start_pos) / 2)
 	
-	start_pos_bottom = a + Vector2(0, 50)
-	end_pos_bottom = b + Vector2(0, 50)
+	self.rotation = atan2(end_pos.y - start_pos.y, end_pos.x - start_pos.x)
 	
-	$StaticBody2D/Bottom.position = start_pos_bottom + ((end_pos_bottom - start_pos_bottom) / 2)
-	$StaticBody2D/Bottom.rotation = atan2(end_pos_bottom.y - start_pos_bottom.y, end_pos_bottom.x - start_pos_bottom.x)
-	$StaticBody2D/Bottom.scale = Vector2(start_pos_bottom.distance_to(end_pos_bottom)/road_x_size, road_y_scale)
-	
-#	self.position = start_pos + ((end_pos - start_pos) / 2)
-#	self.rotation = atan2(end_pos.y - start_pos.y, end_pos.x - start_pos.x)
+	#var distance = sqrt(pow(end_pos.y - start_pos.y,2) + pow(end_pos.x - start_pos.x,2))
+	var distance = start_pos.distance_to(end_pos)
+	self.scale = Vector2(distance/road_x_size, road_y_scale)
+
 #
-#	#var distance = sqrt(pow(end_pos.y - start_pos.y,2) + pow(end_pos.x - start_pos.x,2))
-#	var distance = start_pos.distance_to(end_pos)
-#	self.scale = Vector2(distance/road_x_size, road_y_scale)
-	
+#func add_road():
+#	var mouse = get_global_mouse_position()
+#
+#	var scale = last_mouse_position.distance_to(mouse) / 125
+#
+#	var new_straight_road = straight_road_scene.instantiate()
+#
+#	new_straight_road.scale = Vector2(scale, 1)
+#	print("Mouse: ", mouse)
+#	print("Last: ", last_mouse_position)
+#	print("Distance:" , last_mouse_position.distance_to(mouse))
+#	print("Scale: ", new_straight_road.scale)
+#
+#	new_straight_road.global_position = mouse
+#	new_straight_road.rotation = atan2(last_mouse_position.y - mouse.y, last_mouse_position.x - mouse.x)
+#
+#	roads.add_child(new_straight_road)
+#	roads.move_child(new_straight_road, -1)
+#
+#	last_mouse_position = mouse
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
