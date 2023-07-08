51cf5f3ef5972224fc5f6810185404df436437e9
Joshua Bourgeot <dragonsploder@gmail.com>
2023-07-07 19:53:32 -0700

    kinda does what we want now

--- Map.gd
+++ Map.gd
@@ -1,44 +1,43 @@
 extends Node2D
 
 @export var straight_road_scene: PackedScene
-@onready var straight_road = straight_road_scene.instantiate()
-@onready var roads = find_child("Roads")
+#@onready var straight_road = straight_road_scene.instantiate()
+#@onready var roads = find_child("Roads")
+signal send_points_to_road(a: Vector2, b: Vector2)
+
+var old_mouse_pos = Vector2(300,300)
+
+func add_road(a, b):
+	var straight_road = straight_road_scene.instantiate()
+	print(straight_road)
+	add_child(straight_road)
+	move_child(straight_road, 0)
+	send_points_to_road.connect(straight_road.on_send_points_to_road)
+	send_points_to_road.emit(a, b)
+	send_points_to_road.disconnect(straight_road.on_send_points_to_road)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	straight_road.global_position = Vector2(600,600)
-	roads.add_child(straight_road)
-	roads.move_child(straight_road, -1)
-
-
-var last_mouse_position = Vector2(600, 600 + 125)
-
-func add_road():
-	var mouse = get_global_mouse_position()
-	
-	var scale = last_mouse_position.distance_to(mouse) / 125
-	
-	var new_straight_road = straight_road_scene.instantiate()
-	
-	new_straight_road.scale = Vector2(scale, 1)
-	print("Mouse: ", mouse)
-	print("Last: ", last_mouse_position)
-	print("Distance:" , last_mouse_position.distance_to(mouse))
-	print("Scale: ", new_straight_road.scale)
-	
-	new_straight_road.global_position = mouse
-	new_straight_road.rotation = atan2(last_mouse_position.y - mouse.y, last_mouse_position.x - mouse.x)
-
-	roads.add_child(new_straight_road)
-	roads.move_child(new_straight_road, -1)
-	
-	last_mouse_position = mouse
+	#straight_road.global_position = Vector2(600,600)
+	#roads.add_child(straight_road)
+	#roads.move_child(straight_road, -1)
+	add_road(Vector2(0, old_mouse_pos.y), old_mouse_pos)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			add_road()
+#	if event is InputEventMouseButton:
+#		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+#			add_road(old_mouse_pos, get_global_mouse_position())
+#			old_mouse_pos = get_global_mouse_position()
+	if get_global_mouse_position().distance_to(old_mouse_pos) > 50:
+		add_road(old_mouse_pos, get_global_mouse_position())
+		old_mouse_pos = get_global_mouse_position()
+
+
+func _on_spawn_road_timeout():
+	pass
+	#add_road(old_mouse_pos, get_global_mouse_position())
+	#old_mouse_pos = get_global_mouse_position()s
