678a1a6a6608d8e9e4f5d708bc49075f11f48e42
OrestesK <oresteskassian@gmail.com>
2023-07-08 13:39:11 -0400

    straight road

--- TerrainManager.gd
+++ TerrainManager.gd
@@ -1,22 +1,24 @@
-extends Area2D
+extends CollisionShape2D
 
 @export var terrain_scene: PackedScene
 
-var chance = 50
+var chance = 100
+var radius_x = 800
+var radius_y = 3000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.global_position = Vector2(5000, 0)
 
 func spawn_terrain():
-	var spawn_area = $CollisionShape2D.shape.extents
-	var origin = $CollisionShape2D.global_position - spawn_area
+	var spawn_area = self.global_position
+	
 	var terrain = terrain_scene.instantiate()
 	
-	var x = randf_range(origin.x, spawn_area.x)
-	var y = randf_range(origin.y, spawn_area.y)
+	var x = randf_range(spawn_area.x - radius_x, spawn_area.x + radius_x)
+	var y = randf_range(spawn_area.y - radius_y, spawn_area.y  + radius_y)
 	
-	terrain.position = Vector2(x, y)
+	terrain.global_position = Vector2(x, y)
 	
 	var scale = randf_range(0.3, 1.3)
 	terrain.scale = Vector2(scale, scale)
