a7389ec48b8bb0cc36d874170a73c7753c6443bf
Joshua Bourgeot <joshuabourgeot@gmail.com>
2023-07-08 01:36:17 -0700

    try it

---
+++ CharacterBody2D.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+var initial_velocity = Vector2(300, 0)
+var min_velocity = Vector2(-800, -800)
+var max_velocity = Vector2(800, 800)
+
+func _ready():
+	set_velocity(initial_velocity)
+
+func _physics_process(delta):
+	rotation = velocity.angle()
+	
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
+	
+	#var mouse = get_global_mouse_position()
+	#var mouse = get_tree().get_root().get_node("Main").get_child("Camera").position
+	
+	var bias = 300 * delta
+	#var pos_diff = (mouse - global_position).normalized()
+	var pos_diff = Vector2(1,0)
+
+	velocity += pos_diff * bias
+	velocity = velocity.clamp(min_velocity, max_velocity)
--- main.gd
+++ main.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var up_road: PackedScene
 @export var down_road: PackedScene
 
+@export var moon_road: PackedScene
+
 var current_marker_loc = Vector2(0,500)
 
 
@@ -14,6 +16,9 @@ signal update_camera_target(loc: Vector2)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_camera_target.connect($Camera2D.on_update_camera_target)
+	current_marker_loc = $SpawnArea/SpawnShape.position + Vector2(50, 50)
+	place_road(straight_road)
+	$SpawnArea/SpawnShape.spawn_cars()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,10 +29,13 @@ func _process(delta):
 		place_road(up_road)
 	if Input.is_action_just_pressed("ui_down"):
 		place_road(down_road)
+	if Input.is_action_just_pressed("ui_left"):
+		place_road(moon_road)
 
 func place_road(road: PackedScene):
 	var road_instance = road.instantiate()
 	add_child(road_instance)
+	move_child(road_instance, 0)
 	
 	send_point_to_road.connect(road_instance.on_send_point_to_road)
 	send_point_to_road.emit(current_marker_loc)
